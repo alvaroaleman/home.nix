96078bdef5f2ba87e4c78c5f1c48574c543fb51a
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2025-09-14 21:01:58 -0400

    Make diff script less bad

--- hack/diff.sh
+++ hack/diff.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CURRENT="$(home-manager generations | head -1 | awk '{print $7}')"
-PREVIOUS="$(home-manager generations | head -2 | tail -1| awk '{print $7}')"
+CURRENT="$(home-manager generations | grep 'current' | awk '{print $7}')"
+PREVIOUS="$(home-manager generations | grep 'current' -A1 | tail -1| awk '{print $7}')"
 
 nix store diff-closures "$PREVIOUS" "$CURRENT"
