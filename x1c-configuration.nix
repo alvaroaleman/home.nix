7d2e9914b3af0280c30fa7c80954302ad99d7b6f
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2025-10-01 18:14:16 -0400

    Improve power management

--- x1c-configuration.nix
+++ x1c-configuration.nix
@@ -135,4 +135,26 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+
+  powerManagement.powertop.enable = true; # enable powertop auto tuning on startup.
+  services.system76-scheduler.settings.cfsProfiles.enable = true; # Better scheduling for CPU cycles - thanks System76!!!
+  services.thermald.enable = true; # Enable thermald, the temperature management daemon. (only necessary if on Intel CPUs)
+  services.power-profiles-daemon.enable = false; # Disable GNOMEs power management
+  services.tlp = {
+    enable = true; # Enable TLP (better than gnomes internal power manager)
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 1;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 1;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 81;
+    };
+  };
 }
